1fe9dda64c0232069b53c084a9128d9a1bf4623f
Jesse Benedict <ben@gruber.com>
2025-06-20 07:43:17 -0700

    final design changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,27 +14,57 @@
             return;
         }
         
-        // Handle shipping method display
+        // Handle shipping method display and grouping
         function enhanceShippingMethods() {
-            $('.shipping_method').each(function() {
-                if ($(this).val() && $(this).val().indexOf('shippo_live_rates') >= 0) {
-                    var $label = $(this).closest('li').find('label');
+            // First, find all Shippo rates
+            var $shippoRates = $('.shipping_method').filter(function() {
+                return $(this).val() && $(this).val().indexOf('shippo_live_rates') >= 0;
+            });
+            
+            if ($shippoRates.length === 0) {
+                return; // No Shippo rates found
+            }
+            
+            // Group rates by carrier
+            var carriers = {};
+            var currentCarrier = '';
+            
+            $shippoRates.each(function() {
+                var $label = $(this).closest('li').find('label');
+                var labelText = $label.text();
+                var carrierMatch = labelText.match(/^(UPS|USPS|FedEx)/i);
+                
+                if (carrierMatch) {
+                    var carrier = carrierMatch[0];
                     
-                    // Only add class if not already added
-                    if (!$label.hasClass('wc-shippo-live-rate')) {
-                        $label.addClass('wc-shippo-live-rate');
-                        
-                        // Identify carrier for possible styling
-                        var labelText = $label.text().toLowerCase();
-                        if (labelText.indexOf('usps') >= 0) {
-                            $label.addClass('wc-shippo-carrier-usps');
-                        } else if (labelText.indexOf('ups') >= 0) {
-                            $label.addClass('wc-shippo-carrier-ups');
-                        } else if (labelText.indexOf('fedex') >= 0) {
-                            $label.addClass('wc-shippo-carrier-fedex');
-                        }
+                    if (!carriers[carrier]) {
+                        carriers[carrier] = [];
+                    }
+                    
+                    carriers[carrier].push($(this).closest('li'));
+                    
+                    // Add carrier class to the label
+                    $label.addClass('wc-shippo-live-rate');
+                    
+                    if (labelText.toLowerCase().indexOf('usps') >= 0) {
+                        $label.addClass('wc-shippo-carrier-usps');
+                    } else if (labelText.toLowerCase().indexOf('ups') >= 0) {
+                        $label.addClass('wc-shippo-carrier-ups');
+                    } else if (labelText.toLowerCase().indexOf('fedex') >= 0) {
+                        $label.addClass('wc-shippo-carrier-fedex');
                     }
                 }
+            });
+            
+            // Add carrier headers and organize groups
+            Object.keys(carriers).forEach(function(carrier) {
+                var $firstItem = carriers[carrier][0];
+                var headerHtml = '<li class="wc-shippo-carrier-header"><span>' + carrier + ' Shipping Options</span></li>';
+                $firstItem.before(headerHtml);
+                
+                // Add separator after last item in group (except the last group)
+                var $lastItem = carriers[carrier][carriers[carrier].length - 1];
+                $lastItem.addClass('wc-shippo-last-in-group');
             });
         }
         
